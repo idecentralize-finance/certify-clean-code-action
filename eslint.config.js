8b6add93adb1509269c889e2b5a4cbd3c314ee86
madeindreams <ian@idecentralize.finance>
2024-09-25 18:37:50 -0400

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,22 @@
 // eslint.config.js
-module.exports = [
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended, // Use the recommended settings from ESLint
+
     {
         files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest', // Specifies the ECMAScript version
+            sourceType: 'module',  // Indicates that we're using ES modules
+            globals: {             // Define any global variables here
+                // Add custom global variables here if needed, e.g.,
+                // myGlobalVar: 'readonly',
+            },
+        },
         rules: {
             'no-console': 'off',
             'semi': ['error', 'always'],
-        },
-        env: {
-            node: true,
-            es2020: true,
-        },
-        parserOptions: {
-            ecmaVersion: 2020,
         },
     },
 ];
