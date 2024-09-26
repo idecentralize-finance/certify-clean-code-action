60c810adc9cbedf8c7a43b291fcdde8eeee0d396
madeindreams <ian@idecentralize.finance>
2024-09-26 14:14:52 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -31101,22 +31101,14 @@ const core = __nccwpck_require__(7484);
 const github = __nccwpck_require__(3228);
 const path = __nccwpck_require__(6928);
 const { execSync } = __nccwpck_require__(5317);
-const { fileURLToPath } = __nccwpck_require__(7016);
-
-// Fix __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Define the path to the ESLint configuration relative to the Action's directory
-const eslintConfigPath = path.join(__dirname, 'eslint.config.js'); // Make sure this matches your ESLint config filename
+const eslintConfigPath = __nccwpck_require__.ab + "eslint.config.js"; // Ensure this matches your ESLint config filename
 
 async function run() {
     try {
-        // Set up the scanning tools or run your custom scanning logic
-        execSync(`npx eslint --config ${eslintConfigPath} --ext .js .`, { stdio: 'inherit' });
-
-
-        // Add more scanning steps as needed
+        // Run ESLint with the correct configuration file
+        execSync(`npx eslint --config "${eslintConfigPath}" --ext .js .`, { stdio: 'inherit' });
 
         // Set the output of the action
         core.setOutput("result", "No malicious code detected");
